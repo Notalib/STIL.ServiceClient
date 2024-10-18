346b00b0898be61cb414c0f84f70e39e8e9afa9a
Henrik Sørensen <hsn@nota.dk>
2024-10-18 02:31:16 +0200

    fix: IPL is very particular about GetOrdrerQuery's datetime serialisation.

--- src/STIL.ServiceClient/DTOs/SPSA/GetOrdrer/GetOrdrerQuery.cs
+++ src/STIL.ServiceClient/DTOs/SPSA/GetOrdrer/GetOrdrerQuery.cs
@@ -8,9 +8,20 @@ namespace STIL.ServiceClient.DTOs.SPSA.GetOrdrer;
 [XmlType(AnonymousType = true, Namespace = "http://stil.dk/spsa/ordreservice/v1.0")]
 public class GetOrdrerQuery
 {
+    public GetOrdrerQuery()
+    {
+        fromTime = toTime = null;
+    }
+
+    public GetOrdrerQuery(DateTime? from = null, DateTime? to = null)
+    {
+        fromTime = from?.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss.fff'Z'");
+        toTime = to?.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss.fff'Z'");
+    }
+
     [XmlElement(IsNullable = true, Order = 0)]
-    public DateTime? fromTime { get; set; }
+    public string fromTime { get; set; }
 
     [XmlElement(IsNullable = true, Order = 1)]
-    public DateTime? toTime { get; set; }
+    public string toTime { get; set; }
 }
